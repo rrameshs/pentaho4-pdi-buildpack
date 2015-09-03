26f3494ab1634401d05f08ada93a50c1eb656984
mabeveridge <mabeveridge@users.noreply.github.com>
2015-09-03 17:35:01 +0100

    test

--- profile/setenv.sh
+++ profile/setenv.sh
@@ -1,6 +1,2 @@
-#!/usr/bin/env bash
-
-#export JAVA_HOME=$HOME/.pdi-buildpack/open_jdk_jre 
-#export KETTLE_HOME=$HOME
-
-echo "TEST" >> tmp.txt
+export JAVA_HOME=$HOME/.pdi-buildpack/open_jdk_jre 
+export KETTLE_HOME=$HOME
