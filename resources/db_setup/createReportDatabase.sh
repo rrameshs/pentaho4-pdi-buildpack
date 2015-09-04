271e327a766ece9af823a62bc113a5425fd700e2
mabeveridge <mabeveridge@users.noreply.github.com>
2015-09-04 18:47:33 +0100

    remove liquibase.properties

--- resources/db_setup/createReportDatabase.sh
+++ resources/db_setup/createReportDatabase.sh
@@ -11,6 +11,6 @@ psql -h $REPORTING_DB_HOST -p $REPORTING_DB_PORT -U $REPORTING_DB_USER -d $REPOR
 psql -h $REPORTING_DB_HOST -p $REPORTING_DB_PORT -U $REPORTING_DB_USER -d $REPORTING_DB_NAME -c "create schema mart;"
 
 echo "Executing DDL ..."
-~/.pdi-buildpack/liquibase/liquibase --username=$REPORTING_DB_USER --password=$REPORTING_DB_PW --url=jdbc:postgresql://$REPORTING_DB_HOST:$REPORTING_DB_PORT/$REPORTING_DB_NAME update
+~/.pdi-buildpack/liquibase/liquibase --username=$REPORTING_DB_USER --password=$REPORTING_DB_PW --url=jdbc:postgresql://$REPORTING_DB_HOST:$REPORTING_DB_PORT/$REPORTING_DB_NAME --changeLogFile=$HOME/.pdi-buildpack/db_setup/reportdb_template/changelog-master.xml --classpath=$HOME/.pdi-buildpack/pdi/data-integration/libext/JDBC/postgresql-8.4-702.jdbc3.jar update
 
 echo "Done"
