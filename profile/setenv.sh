5fe6e9f56819a4fa0f00b2e5a9782d0072163f32
mabeveridge <mabeveridge@users.noreply.github.com>
2015-09-07 12:55:42 +0100

    test

--- profile/setenv.sh
+++ profile/setenv.sh
@@ -1,3 +1,3 @@
-export PATH=$PATH:$HOME/.pdi-buildpack/liquibase
-export JAVA_HOME=$HOME/.pdi-buildpack/open_jdk_jre 
 export KETTLE_HOME=$HOME
+export JAVA_HOME=$HOME/.pdi-buildpack/open_jdk_jre 
+export PATH=$PATH:$JAVA_HOME/bin:$HOME/.pdi-buildpack/liquibase
