5946319df71189ae44375c9060fedcc375d36350
mabeveridge <mabeveridge@users.noreply.github.com>
2015-09-07 17:03:08 +0100

    test

--- lib/database.sh
+++ lib/database.sh
@@ -9,7 +9,7 @@ create_db() {
 	export PGPASSWORD=$REPORTING_DB_PW
 	echo "Creating database ${REPORTING_DB_NAME} on ${REPORTING_DB_HOST} ..."
 	report_db_exists=`psql -h $REPORTING_DB_HOST -p $REPORTING_DB_PORT -U $REPORTING_DB_USER -l | grep $REPORTING_DB_NAME | wc -l`
-	if [$report_db_exists -eq 1]; then
+	if [[ $report_db_exists -eq 1 ]]; then
 		echo "Database already exists"
 	else
 		psql -h $REPORTING_DB_HOST -p $REPORTING_DB_PORT -U $REPORTING_DB_USER -d postgres -c "create database $REPORTING_DB_NAME;"
@@ -17,7 +17,7 @@ create_db() {
 	
 	echo "Creating dw schema"
 	dw_exists=`psql -h $REPORTING_DB_HOST -p $REPORTING_DB_PORT -U $REPORTING_DB_USER -d $REPORTING_DB_NAME -c "select 'DW_EXISTS' from pg_namespace where nspname = 'dw';" | grep 'DW_EXISTS' | wc -l`
-	if [$dw_exists -eq 1]; then
+	if [[ $dw_exists -eq 1 ]]; then
 		echo "Schema already exists"
 	else
 		psql -h $REPORTING_DB_HOST -p $REPORTING_DB_PORT -U $REPORTING_DB_USER -d $REPORTING_DB_NAME -c "create schema if not exists dw;"
@@ -25,7 +25,7 @@ create_db() {
 	
 	echo "Creating mart schema"
 	mart_exists=`psql -h $REPORTING_DB_HOST -p $REPORTING_DB_PORT -U $REPORTING_DB_USER -d $REPORTING_DB_NAME -c "select 'MART_EXISTS' from pg_namespace where nspname = 'mart';" | grep 'MART_EXISTS' | wc -l`
-	if [$mart_exists -eq 1]; then
+	if [[ $mart_exists -eq 1 ]]; then
 		echo "Schema already exists"
 	else
 		psql -h $REPORTING_DB_HOST -p $REPORTING_DB_PORT -U $REPORTING_DB_USER -d $REPORTING_DB_NAME -c "create schema if not exists mart;"
@@ -42,7 +42,7 @@ create_db() {
 		echo "Creating database ${dbname} on ${dbhost} ..."
 		export PGPASSWORD=$dbpwd
 		local db_exists=`psql -h $dbhost -p $dbport -U $dbuser -l | grep $dbname | wc -l`
-		if [$db_exists -eq 1]; then
+		if [[ $db_exists -eq 1 ]]; then
 			echo "Database already exists"
 		else
 			psql -h $dbhost -p $dbport -U $dbuser -d postgres -c "create database ${dbname};"
