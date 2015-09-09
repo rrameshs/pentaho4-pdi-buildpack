e9ea10497967afab1e08fee2b1306296ad28a644
mabeveridge <mabeveridge@users.noreply.github.com>
2015-09-09 10:18:45 +0100

    add liquibase home

--- profile/setenv.sh
+++ profile/setenv.sh
@@ -1,3 +1,4 @@
 export KETTLE_HOME=$HOME
 export JAVA_HOME=$HOME/.pdi-buildpack/open_jdk_jre 
-export PATH=$PATH:$JAVA_HOME/bin:$HOME/.pdi-buildpack/liquibase
+export LIQUIBASE_HOME=$HOME/.pdi-buildpack/liquibase
+export PATH=$PATH:$JAVA_HOME/bin:$LIQUIBASE_HOME
