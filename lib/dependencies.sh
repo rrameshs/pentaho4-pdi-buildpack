9ca81132e2061e8eb5499f5ac26dabf8c2740a6f
mabeveridge <mabeveridge@users.noreply.github.com>
2015-09-09 10:51:22 +0100

    copying pg driver to liquibase

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -12,14 +12,7 @@ install_dependencies() {
 	curl -s ${jdk_url} > ${cache_dir}/openjdk-1.8.0_60.tar.gz
 	mkdir ${build_dir}/.pdi-buildpack/open_jdk_jre
 	tar xzf ${cache_dir}/openjdk-1.8.0_60.tar.gz -C ${build_dir}/.pdi-buildpack/open_jdk_jre
-		
-	# install Liquibase
-	local liquibase_url="https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.4.1/liquibase-3.4.1-bin.tar.gz"
-	echo "Installing ${liquibase_url} to ${build_dir}/.pdi-buildpack/liquibase"
-	curl -s -L ${liquibase_url} > ${cache_dir}/liquibase-3.4.1-bin.tar.gz
-	mkdir ${build_dir}/.pdi-buildpack/liquibase
-	tar xzf ${cache_dir}/liquibase-3.4.1-bin.tar.gz -C ${build_dir}/.pdi-buildpack/liquibase
-	
+			
 	# install Pentaho PDI
 	# http://downloads.sourceforge.net/project/pentaho/Data%20Integration/4.4.0-stable/pdi-ce-4.4.0-stable.tar.gz
 	local pdi_url="https://s3-eu-west-1.amazonaws.com/voxgenbi/pdi-ce-4.4.0-stable.tar.gz"
@@ -27,4 +20,15 @@ install_dependencies() {
 	curl -s -L  ${pdi_url} > ${cache_dir}/pdi-ce-4.4.0-stable.tar.gz
 	mkdir ${build_dir}/.pdi-buildpack/pdi
 	tar xzf ${cache_dir}/pdi-ce-4.4.0-stable.tar.gz -C ${build_dir}/.pdi-buildpack/pdi	
+
+	# install Liquibase
+	local liquibase_url="https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.4.1/liquibase-3.4.1-bin.tar.gz"
+	echo "Installing ${liquibase_url} to ${build_dir}/.pdi-buildpack/liquibase"
+	curl -s -L ${liquibase_url} > ${cache_dir}/liquibase-3.4.1-bin.tar.gz
+	mkdir ${build_dir}/.pdi-buildpack/liquibase
+	tar xzf ${cache_dir}/liquibase-3.4.1-bin.tar.gz -C ${build_dir}/.pdi-buildpack/liquibase
+	
+	# copy postgres jdbc driver from pdi to liquibase
+	cp ${build_dir}/.pdi-buildpack/pdi/data-integration/libext/JDBC/postgresql-*.jar ${build_dir}/.pdi-buildpack/liquibase/lib
+
 }
