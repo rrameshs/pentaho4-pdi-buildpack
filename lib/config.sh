0c281fd25c75c5399283afe1af588c2b0ee5d12e
mabeveridge <mabeveridge@users.noreply.github.com>
2015-09-11 18:41:47 +0100

    Adding support for PDI memory specification

--- lib/config.sh
+++ lib/config.sh
@@ -21,7 +21,15 @@ reconfigure_app() {
 	# add .profile.d script to set environment
 	echo "Setting environment"
 	mkdir -p ${build_dir}/.profile.d
-	cp ${bp_dir}/profile/* ${build_dir}/.profile.d/
+	cp ${bp_dir}/profile/setenv.sh ${build_dir}/.profile.d/
+	
+	# add memory spec to profile if specified in kettle.properties
+	local pdi_mem=$(grep "PDI_MAX_MEMORY" ${build_dir}/.kettle/kettle.properties | cut -d = -f 2)
+	if [[ ! -z "$pdi_mem" ]]; then
+		local pdi_java_opts="PENTAHO_DI_JAVA_OPTS=-Xmx${pdi_mem}"
+		echo "Setting ${pdi_java_opts}" | indent
+		echo "export ${pdi_java_opts}" >> ${build_dir}/.profile.d/setenv.sh
+	fi
 	
 	#edit pentaho env script to source .profile.d
 	echo "Updating set-pentaho-env script"
