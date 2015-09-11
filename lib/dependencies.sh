694e35b8ba6db0534529146de2d4de96ded82472
mabeveridge <mabeveridge@users.noreply.github.com>
2015-09-11 11:04:45 +0100

    Added support for offline package

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -3,32 +3,46 @@ install_dependencies() {
 	local build_dir=$1
 	local cache_dir=$2	
 	local bp_dir=$3
-	
-	# create buildpack dir
-	mkdir -p ${build_dir}/.pdi-buildpack
-	
+		
 	# load dependencies properties
 	source ${bp_dir}/config/dependencies.properties
 	
 	# install Java
-	echo "Installing ${jdk_url} to ${build_dir}/.pdi-buildpack/open_jdk_jre"
-	curl -s ${jdk_url} > ${cache_dir}/openjdk-1.8.0_60.tar.gz
-	mkdir ${build_dir}/.pdi-buildpack/open_jdk_jre
-	tar xzf ${cache_dir}/openjdk-1.8.0_60.tar.gz -C ${build_dir}/.pdi-buildpack/open_jdk_jre
+	local jdk_dir=${build_dir}/.pdi-buildpack/open_jdk_jre
+	mkdir -p ${jdk_dir}
+	install_package $bp_dir $cache_dir $jdk_url $jdk_dir
 			
 	# install Pentaho PDI	
-	echo "Installing ${pdi_url} to ${build_dir}/.pdi-buildpack/pdi"	
-	curl -s -L  ${pdi_url} > ${cache_dir}/pdi-ce-4.4.0-stable.tar.gz
-	mkdir ${build_dir}/.pdi-buildpack/pdi
-	tar xzf ${cache_dir}/pdi-ce-4.4.0-stable.tar.gz -C ${build_dir}/.pdi-buildpack/pdi	
+	local pdi_dir=${build_dir}/.pdi-buildpack/pdi"
+	mkdir -p ${pdi_dir}
+	install_package $bp_dir $cache_dir $pdi_url $pdi_dir	
 
 	# install Liquibase
-	echo "Installing ${liquibase_url} to ${build_dir}/.pdi-buildpack/liquibase"
-	curl -s -L ${liquibase_url} > ${cache_dir}/liquibase-3.4.1-bin.tar.gz
-	mkdir ${build_dir}/.pdi-buildpack/liquibase
-	tar xzf ${cache_dir}/liquibase-3.4.1-bin.tar.gz -C ${build_dir}/.pdi-buildpack/liquibase
+	local liquibase_dir=${build_dir}/.pdi-buildpack/liquibase"
+	mkdir -p ${liquibase_dir}
+	install_package $bp_dir $cache_dir $liquibase_url $liquibase_dir
 	
 	# copy postgres jdbc driver from pdi to liquibase
-	cp ${build_dir}/.pdi-buildpack/pdi/data-integration/libext/JDBC/postgresql-*.jar ${build_dir}/.pdi-buildpack/liquibase/lib
+	cp ${pdi_dir}/data-integration/libext/JDBC/postgresql-*.jar ${liquibase_dir}/lib
+}
 
+install_package() {
+
+	local bp_dir=$1
+	local cache_dir=$2
+	local pkg_url=$3
+	local pkg_dir=$4
+	
+	echo "Installing ${pkg_url} to ${pkg_dir}"
+	
+	# get the package, either from buildpack or url
+	local pkg_file=${pkg_url##*/}
+	if [[ -f ${bp_dir}/dependencies/${pkg_file} ]]; then
+		cp ${bp_dir}/dependencies/${pkg_file} ${cache_dir}/${pkg_file}
+	else
+		curl -s -L ${pkg_url} > ${cache_dir}/${pkg_file}
+	fi				
+	
+	# unpack it into the target dir
+	tar xzf ${cache_dir}/${pkg_file} -C ${pkg_dir}
 }
